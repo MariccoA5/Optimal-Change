1eab0ee6409805d43020e7bd5668b383cf0cd214
MELO <MariccoA5@gmail.com>
2023-01-19 12:45:36 -0800

    bug fix and repo update.

---
+++ optimal_change.py
@@ -0,0 +1,87 @@
+#Money change Algorithm
+
+def optimal_change(cost, paid):
+  
+  if paid == cost: #If no change
+    return f'The optimal change for an item that costs ${cost} with an amount paid of ${paid} is no change given.'
+  
+  if paid < cost: #If paid less than cost
+    return 'Not enough money for the item.'
+  
+  change = paid - cost 
+  
+  bills = { 
+    '100': 0, '50': 0, '20': 0, '10': 0, '5': 0, '1': 0, 'quarter': 0, 'dime': 0, 'nickel': 0, 'penny': 0
+         }
+  
+  def helper(key, value, change):
+    bills[key] += 1
+    change = round(change - value, 2)
+    return change
+      
+  while (change > 0): # update bills dict to match the change
+    if change > 100:
+      change = helper('100', 100.00, change)
+      continue
+    if change >=  50:
+      change = helper('50', 50.00, change)
+      continue
+    if change >=  20:
+      change = helper('20', 20.00, change)
+      continue
+    if change >=  10:
+      change = helper('10', 10.00, change)
+      continue
+    if change >=  5:
+      change = helper('5', 5.00, change)
+      continue
+    if change >=  1:
+      change = helper('1', 1.00, change)
+      continue
+    if change >=  .25:
+      change = helper('quarter', 0.25, change)
+      continue
+    if change >=  .1:
+      change = helper('dime', 0.10, change)
+      continue
+    if change >=  .05:
+      change = helper('nickel', 0.05, change)
+      continue
+    if change >  0:
+      change = helper('penny', 0.01, change)
+      continue
+   
+  return optimal_bills(bills, cost, paid)
+
+#Optimal bills Algorithm
+
+def optimal_bills(bills, cost, paid):
+  
+  str = ''
+  msg = f'The optimal change for an item that costs ${cost} with an amount paid of ${paid} is '
+  
+  for k, v in bills.items(): #key: value pairs
+    if len(k) <= 3: #all the bills
+      if v == 1:  #if only 1 bill
+        msg += f'{v} ${k} bill, '
+        continue
+      if v >= 2:  #if 2 or more bills
+        msg += f'{v} ${k} bills, '
+        continue
+    else: #all the change
+      if k == 'penny': #fix the penny -> pennies problem
+        if v >= 2: #if 2 more pennies
+          msg += f'{v} {k[:-1]}ies, '
+          continue
+      if v == 1: #if single coin
+        msg += f'{v} {k}, '
+        continue
+      if v >= 2: #if multiple of the same coin
+        msg += f'{v} {k}s, '
+  #fixing the final ', and ' problem and adding a '.' at the end of string
+  str = msg[-15:]
+  str = str[:-2] + '.'
+  str = str.replace(', ', ', and ')
+  msg = msg[:-15] + str
+       
+  return msg
---
+++ optimal_change_spec.py
@@ -0,0 +1,18 @@
+from optimal_change import optimal_change
+
+# Write your tests here!
+print("1:", optimal_change(62.13, 100) == "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
+
+print("2:", optimal_change(31.51, 50) == "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
+
+print("3:", optimal_change(0.01, 999) == "The optimal change for an item that costs $0.01 with an amount paid of $999 is 9 $100 bills, 1 $50 bill, 2 $20 bills, 1 $5 bill, 3 $1 bills, 3 quarters, 2 dimes, and 4 pennies.")
+
+print("4:", optimal_change(50, 50.01) == "The optimal change for an item that costs $50 with an amount paid of $50.01 is 1 penny.")
+
+print("5:", optimal_change(100, 100) == "The optimal change for an item that costs $100 with an amount paid of $100 is no change given.")
+
+print("6:", optimal_change(29.41, 34.76) == "The optimal change for an item that costs $29.41 with an amount paid of $34.76 is 1 $5 bill, 1 quarter, and 1 dime.")
+
+print("7:", optimal_change(1.98, 1.00) == "Not enough money for the item.")
+
+print("8:", optimal_change(29.41, 34.76) == "The optimal change for an item that costs $29.41 with an amount paid of $34.76 is 1 $5 bill, 1 quarter, and 1 dime.")
